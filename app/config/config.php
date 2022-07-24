d80fcbe75d0d49bcde673f5984ec35e114af6bc3
btkdevkhdelli5 <amcjkbtk91@outlook.com>
2022-07-25 01:57:19 +0200

    added sql file

--- app/config/config.php
+++ app/config/config.php
@@ -3,8 +3,8 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'jimpizza');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123456789');
 
 define('APPROOT', dirname(dirname(__FILE__)));
-define('URLROOT', 'http://localhost/jimpizza');
+define('URLROOT', 'http://localhost/PHP-DEVS/jimpizza');
 define('SITENAME', 'Jim Pizza');
